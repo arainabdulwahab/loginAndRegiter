6d4588b2795f5d1fcd8b15f8a387e96cb15b8a74
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-10-01 18:29:00 +0200

    javascript with value

---
+++ style.js
@@ -0,0 +1,17 @@
+let forms = [
+    {
+        email: "antoniolupin@gmail.com",
+        password: "hacunamatat"
+    },
+    {
+        email: "carlofesta@gmail.com",
+        password: "pastaConSuggo"
+    }
+];
+
+let gmails = '';
+let paswords = '';
+function getUserInfo(){
+    let gmail = document.getElementById("gmail").value;
+    let password = document.getElementById("password").value;
+}
